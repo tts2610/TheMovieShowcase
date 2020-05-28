5402bf124ef6a7e340cbff726128408e9008ee2e
Sean <psytst@nottingham.ac.uk>
2020-05-28 14:15:16 +0700

    first commit

--- .history/src/App_20200528140840.js
+++ .history/src/App_20200528140840.js
@@ -1,7 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-const API_KEY = process.env.REACT_APP_APIKEY;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
