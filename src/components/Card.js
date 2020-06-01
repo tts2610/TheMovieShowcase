0ad8e8de44d678ec689734355c380c8b5480425c
Adenhall <jamesadenhall@gmail.com>
2020-06-01 14:44:57 +0700

    added modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import "aos/dist/aos.css";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import { withStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
+import Modal from "react-bootstrap/Modal";
+import YouTube from "@u-wave/react-youtube";
+
 const StyledRating = withStyles({
   iconFilled: {
     color: "#ff6d75",
@@ -16,6 +19,10 @@ const StyledRating = withStyles({
   },
 })(Rating);
 export default function Card({ movie }) {
+  const [showModal, setShowModal] = useState(false);
+  const [movieTrailer, setMovieTrailer] = useState("")
+  const API_KEY = process.env.REACT_APP_APIKEY;
+
   const genresBtnList = [];
   let genresList = movie.genres;
   genresList.forEach((each, element) => {
@@ -29,6 +36,17 @@ export default function Card({ movie }) {
       </Button>
     );
   });
+
+  const getMovieTrailerId = async (id) => {
+    let url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
+    let data = await fetch(url);
+    let result = await data.json();
+    setMovieTrailer(result.results[0]);
+    console.log(movieTrailer);
+    console.log(result.results[0])
+  };
+
+  
 
   // useEffect(() => {
   //   Aos.init({ duration: 500, once: true, mirror:true });
@@ -58,10 +76,39 @@ export default function Card({ movie }) {
           <i className="far fa-calendar-edit"></i> {movie.release_date}
         </p>
         <p className="genres">{genresBtnList}</p>
-        <a href="/" onClick={(e) => e.preventDefault()}>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            setShowModal(true);
+            getMovieTrailerId(movie.id);
+          }}
+        >
           View more
         </a>
       </figcaption>
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showModal}
+        onHide={() => setShowModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {movie.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        
+          <p>
+            <YouTube id="trailer" video={movieTrailer.key} width="480px" height="340px" autoplay />
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setShowModal(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </figure>
   );
 }
